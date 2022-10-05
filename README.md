2a12960b29cd89ab5fbb22481e604ad7e45e8271
tmge01 <yos.kajimoto@gmail.com>
2022-10-05 15:51:05 +0900

    DOM操作追加

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # JavaScript勉強用
+
+## DOMの取得
+
+### getElementById
+指定したIDのDOM情報を取得
+
+### getElementsByClassName
+指定したclassのDOM情報を取得
+HTMLCollection(動的)
+
+### querySelector
+指定したセレクタ―で一番最初に一致する要素を返す
+idの場合#とつける
+classの場合は.をつける
+NodeList(静的)
+
+### querySelectorAll
+複数取りたい場合はquerySelectorAllを使う
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ console.log(title1);
 const title2 = document.querySelector("#title");
 console.log(title1);
 
-const title3 = document.querySelector(".container");
-console.log(title3);
+const containers = document.getElementsByClassName("container")
+console.log(containers);
+
+const containers_all = document.querySelectorAll(".container")
+console.log(containers_all);
+
+const container = document.querySelector(".container")
+console.log(container);
+
